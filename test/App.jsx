570311d7f85116662418453fbeadc48f623247e6
Justin Matthew Lee <justin.matthew.lee.18@gmail.com>
2025-02-04 17:21:05 -0500

    a

---
+++ test/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyComponent from "./test";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ test/test.jsx
@@ -0,0 +1,356 @@
+<div
+  style={{
+    width: 1920,
+    height: 1080,
+    position: "relative",
+    background: "white",
+  }}
+>
+  <div
+    style={{
+      width: 1920,
+      height: 1080,
+      left: 0,
+      top: 0,
+      position: "absolute",
+      background: "#F8F8F8",
+    }}
+  />
+  <div
+    style={{
+      width: 1653,
+      height: 930,
+      left: 133,
+      top: 75,
+      position: "absolute",
+    }}
+  >
+    <div
+      style={{
+        width: 1653,
+        height: 930,
+        left: 0,
+        top: 0,
+        position: "absolute",
+        background: "white",
+        boxShadow: "0px 4px 10px 8px rgba(0, 0, 0, 0.25)",
+        borderRadius: 30,
+      }}
+    />
+    <div
+      style={{
+        width: 510,
+        height: 67,
+        left: 998,
+        top: 555,
+        position: "absolute",
+        background: "#31572C",
+        borderRadius: 15,
+      }}
+    />
+    <div
+      style={{
+        width: 856,
+        height: 930,
+        left: 0,
+        top: 0,
+        position: "absolute",
+        background: "#31572C",
+        borderRadius: 30,
+      }}
+    />
+    <div
+      style={{
+        width: 289,
+        height: 67,
+        left: 1108,
+        top: 691,
+        position: "absolute",
+        background: "white",
+        borderRadius: 15,
+        border: "1.25px black solid",
+      }}
+    />
+    <div
+      style={{
+        width: 526,
+        height: 48,
+        left: 997,
+        top: 272,
+        position: "absolute",
+        textAlign: "center",
+        color: "black",
+        fontSize: 50,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      Sign In To Clarus
+    </div>
+    <div
+      style={{
+        width: 665,
+        height: 48,
+        left: 920,
+        top: 776,
+        position: "absolute",
+        textAlign: "center",
+      }}
+    >
+      <span style="color: 'black', fontSize: 19, fontFamily: 'Be Vietnam Pro', fontWeight: '400', wordWrap: 'break-word'">
+        Don’t have an account?{" "}
+      </span>
+      <span style="color: '#31572C', fontSize: 19, fontFamily: 'Be Vietnam Pro', fontWeight: '700', wordWrap: 'break-word'">
+        Register
+      </span>
+    </div>
+    <div
+      style={{
+        width: 195,
+        height: 1,
+        left: 1185,
+        top: 724,
+        position: "absolute",
+        color: "black",
+        fontSize: 19,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      Sign in with Google
+    </div>
+    <div
+      style={{
+        width: 283,
+        height: 47,
+        left: 1118,
+        top: 566,
+        position: "absolute",
+        textAlign: "center",
+        color: "white",
+        fontSize: 19,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      Sign in
+    </div>
+    <div
+      style={{
+        width: 510,
+        height: 67,
+        left: 998,
+        top: 463,
+        position: "absolute",
+        background: "#F3F3F3",
+        borderRadius: 15,
+      }}
+    />
+    <div
+      style={{
+        width: 283,
+        height: 47,
+        left: 1026,
+        top: 473,
+        position: "absolute",
+        color: "#808080",
+        fontSize: 19,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      Password
+    </div>
+    <div
+      style={{
+        width: 510,
+        height: 67,
+        left: 998,
+        top: 382,
+        position: "absolute",
+        background: "#F3F3F3",
+        borderRadius: 15,
+      }}
+    />
+    <div
+      style={{
+        width: 283,
+        height: 48,
+        left: 1026,
+        top: 392,
+        position: "absolute",
+        color: "#808080",
+        fontSize: 19,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      Email
+    </div>
+    <img
+      style={{
+        width: 38,
+        height: 37,
+        left: 1139,
+        top: 706,
+        position: "absolute",
+      }}
+      src="https://via.placeholder.com/38x37"
+    />
+    <div
+      style={{
+        width: 232,
+        height: 0,
+        left: 1276,
+        top: 657,
+        position: "absolute",
+        background: "#808080",
+        border: "1px #808080 solid",
+      }}
+    ></div>
+    <div
+      style={{
+        width: 232,
+        height: 0,
+        left: 998,
+        top: 657,
+        position: "absolute",
+        background: "#808080",
+        border: "1px #808080 solid",
+      }}
+    ></div>
+    <div
+      style={{
+        width: 30,
+        height: 25,
+        left: 1238,
+        top: 645,
+        position: "absolute",
+        textAlign: "center",
+        color: "#808080",
+        fontSize: 12,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      OR
+    </div>
+    <img
+      style={{
+        width: 137,
+        height: 137,
+        left: 1184,
+        top: 106,
+        position: "absolute",
+      }}
+      src="https://via.placeholder.com/137x137"
+    />
+    <div
+      style={{
+        width: 347,
+        height: 51,
+        left: 254,
+        top: 84,
+        position: "absolute",
+        background: "#132A13",
+        borderRadius: 15,
+      }}
+    />
+    <div
+      style={{
+        width: 235,
+        height: 35,
+        left: 310,
+        top: 92,
+        position: "absolute",
+        textAlign: "center",
+        color: "white",
+        fontSize: 19,
+        fontFamily: "Be Vietnam Pro",
+        fontWeight: "700",
+        wordWrap: "break-word",
+      }}
+    >
+      What’s Happening?
+    </div>
+    <img
+      style={{
+        width: 517,
+        height: 449,
+        left: 169,
+        top: 180,
+        position: "absolute",
+        borderRadius: 10,
+      }}
+      src="https://via.placeholder.com/517x449"
+    />
+    <div
+      style={{
+        width: 396,
+        height: 124,
+        left: 230,
+        top: 663,
+        position: "absolute",
+        textAlign: "center",
+      }}
+    >
+      <span style="color: 'white', fontSize: 40, fontFamily: 'Be Vietnam Pro', fontWeight: '700', wordWrap: 'break-word'">
+        Take Action
+        <br />
+      </span>
+      <span style="color: 'white', fontSize: 19, fontFamily: 'Be Vietnam Pro', fontWeight: '700', wordWrap: 'break-word'">
+        <br />
+      </span>
+      <span style="color: 'white', fontSize: 19, fontFamily: 'Be Vietnam Pro', fontWeight: '400', wordWrap: 'break-word'">
+        View a list of practical steps to address an issue discussed in each
+        article
+      </span>
+    </div>
+    <div
+      style={{
+        width: 78,
+        height: 0,
+        left: 363,
+        top: 839,
+        position: "absolute",
+        border: "5px white solid",
+      }}
+    ></div>
+    <div
+      style={{
+        width: 36,
+        height: 0,
+        left: 456,
+        top: 838,
+        position: "absolute",
+        border: "2px #AAAAAA solid",
+      }}
+    ></div>
+    <div
+      style={{
+        width: 36,
+        height: 0,
+        left: 312,
+        top: 838,
+        position: "absolute",
+        border: "2px #AAAAAA solid",
+      }}
+    ></div>
+    <div
+      style={{
+        width: 36,
+        height: 0,
+        left: 507,
+        top: 838,
+        position: "absolute",
+        border: "2px #AAAAAA solid",
+      }}
+    ></div>
+  </div>
+</div>;
